41d94a1acc0daa039a2ba8985e995fe8d7c65967
maxbmn <62843036+maxbmn@users.noreply.github.com>
2021-03-04 18:06:33 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -5,7 +5,7 @@ household_power_consumption<-daten[daten$Date %in% c("1/2/2007","2/2/2007"),]
 #plot construction
 datetime<-strptime(paste(household_power_consumption$Date,household_power_consumption$Time,sp=" "), "%d/%m/%Y %H:%M:%S")
 png("plot3.png",height = 480,width = 480)
-
+#German names for "Thursday", "Friday","Saturday" --> "Do","Fr","Sa"
 plot(datetime,household_power_consumption$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
 lines(datetime,household_power_consumption$Sub_metering_2,col="red")
 lines(datetime,household_power_consumption$Sub_metering_3,col="blue")

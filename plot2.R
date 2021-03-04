fcf7b7e799de7f3b97bf5e85793ecd0852d690f7
maxbmn <62843036+maxbmn@users.noreply.github.com>
2021-03-04 18:06:16 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ household_power_consumption<-daten[daten$Date %in% c("1/2/2007","2/2/2007"),]
 #plot construction
 datetime<-strptime(paste(household_power_consumption$Date,household_power_consumption$Time,sp=" "), "%d/%m/%Y %H:%M:%S")
 png("plot2.png",height = 480,width = 480)
-
+#German names for "Thursday", "Friday","Saturday" --> "Do","Fr","Sa"
 plot(datetime,household_power_consumption$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 dev.off()
